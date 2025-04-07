50d49eb2ef985726cbad2995e240b8b921122621
Sai Rithwik Kukunuri <138318585+YB-Yottabyte@users.noreply.github.com>
2025-04-07 00:13:10 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "./components/ui/sonner";
 import { WeatherDashboard } from "./pages/weather-dashboard";
 import { Layout } from "./components/layout";
